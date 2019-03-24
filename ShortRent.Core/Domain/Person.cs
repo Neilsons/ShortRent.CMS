4727819dd6f98c7001f733b9bfc055612cfcbddf
neilsons <neilsons1015@163.com>
2019-03-24 20:52:06 +0800

    添加实体(#4)

--- ShortRent.Core/Domain/Person.cs
+++ ShortRent.Core/Domain/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace ShortRent.Core.Domain
 {
-    public class Person:BaseEntity
+    public class Person : BaseEntity
     {
         /// <summary>
         /// 姓名
@@ -21,5 +22,40 @@ namespace ShortRent.Core.Domain
         /// null保密 true 男 false 女
         /// </summary>
         public bool? Sex { get; set; }
+        /// <summary>
+        /// 用户类别 0代表前台用户，1 代表后台用户
+        /// </summary>
+        public bool Type { get; set; }
+        /// <summary>
+        /// 个人头像
+        /// </summary>
+        public string PerImage { get; set; }
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string PassWord{get;set;}
+        /// <summary>
+        /// 信用积分
+        /// </summary>
+        [DefaultValue(100)]
+        public int CreditScore { get; set; }
+        /// <summary>
+        /// 身份证号
+        /// </summary>
+        public string IdCard { get; set; }
+        /// <summary>
+        /// 用户排序
+        /// </summary>
+        public int PerOrder { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateTime { get; set; }
+        /// <summary>
+        /// 是否删除 false 不删除 true 删除
+        /// </summary>
+        [DefaultValue(false)]
+        public bool IsDelete { get; set; }
+
     }
 }
--- ShortRent.Data/ShortRentMap/PersonMap.cs
+++ ShortRent.Data/ShortRentMap/PersonMap.cs
@@ -12,8 +12,16 @@ namespace ShortRent.Data.ShortRentMap
     {
         public PersonMap()
         {
+            this.ToTable("Person");
             this.HasKey(c=>c.ID);
             this.Property(c=>c.Name).IsRequired().HasMaxLength(20);
+            this.Property(c => c.CreditScore).IsRequired();
+            this.Property(c=>c.IdCard).HasMaxLength(18).IsRequired();
+            this.Property(c=>c.PassWord).HasMaxLength(120);
+            this.Property(c=>c.PerImage).HasMaxLength(200);
+            this.Property(c=>c.PerOrder).IsOptional();
+
+
         }
     }
 }
