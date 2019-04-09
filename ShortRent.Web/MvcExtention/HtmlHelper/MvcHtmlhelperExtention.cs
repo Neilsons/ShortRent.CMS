f661e91c57ffd82b803af71a6bd9e9cfca551392
neilsons <neilsons1015@163.com>
2019-04-09 23:49:45 +0800

    添加扩展(#23)

---
+++ ShortRent.Web/MvcExtention/HtmlHelper/MvcHtmlhelperExtention.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShortRent.Web.MvcExtention
+{
+    public static class MvcHtmlhelperExtention
+    {
+        public static MvcHtmlString ActionLinkAny(this HtmlHelper htmlHelper, string linkText,string FontIcon,object HtmlAttribute)
+        {
+            string action = htmlHelper.ViewContext.RouteData.Values["action"].ToString();
+            string Controller = htmlHelper.ViewContext.RouteData.Values["controller"].ToString();
+            StringBuilder sbuilder = new StringBuilder();
+            sbuilder.Append("<a href=\"");
+            sbuilder.Append(Controller);
+            sbuilder.Append("/");
+            sbuilder.Append(action);
+            return MvcHtmlString.Create(sbuilder.ToString());
+        }
+    }
+}
